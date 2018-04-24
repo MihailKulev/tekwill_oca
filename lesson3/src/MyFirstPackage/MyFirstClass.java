033850eb0673ffcf548f3cf696b761bd28c2c02a
Mihail Kulev <mihailkulev@gmail.com>
2018-04-24 20:33:15 +0300

    My fist java folder

---
+++ lesson3/src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,12 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+    public static void main (String [] args) {
+     int  total=sum( 1, 2);
+      System.out.print("My First java application");
+    }
+
+public static int sum(int x, int y){
+    return x+y;
+}
+}
